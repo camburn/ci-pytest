7297fe42d973860d6c6ba4f79ccc7eb97447955d
Campbell Blackburn <campbellblackburn@hotmail.com>
2019-12-16 16:57:07 +1100

    New tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -5,3 +5,7 @@ def test_area():
     square = Square(Vector3(1, 1, 1), 3)
     assert square.area() == 9
 
+
+def test_perimeter():
+    square = Square(Vector3(1, 1, 1), 3)
+    assert square.perimeter() == 9
